8b663a48c7365bbaef25ebe58ec32ab15ba88507
hyf <hyf-0418@outlook.com>
2020-05-15 15:04:19 +0800

    add salt

--- backend/login.php
+++ backend/login.php
@@ -1,34 +1,35 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-
   <?php
+
+  include 'PBKDF2.php';
+
   header('Content-type:text/html;charset=utf-8');
   $username =  trim($_POST['username']);
   $password = trim($_POST['password']);
 
-  // 连接数据库:五个参数 mysqli_connect（‘http地址’，数据库用户名，数据库密码，数据库名，端口号）；
   $conn = new mysqli('localhost', 'root', '', 'UserInfo', '3306');
 
   // 检测连接
   if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
+    die("Connect failed: " . $conn->connect_error);
   }
-  echo "连接成功";
+  echo "Connect successfully";
 
-  $sql = "SELECT id, username, password from Authority where username='$username' AND password='$password';";
+  $sql = "SELECT username, hashPassword From Authority where username='$username'";
   $result = mysqli_query($conn, $sql);
-  $row = mysqli_num_rows($result);
 
-  if (!$row) {
-    echo "<script>alert('用户名或密码错误，请重新输入');location='http://127.0.0.1:5500/frontend/src/html/login.html'</script>";
+  if (mysqli_num_rows($result) > 0) {
+    // 输出数据
+    while ($row = mysqli_fetch_assoc($result)) {
+      $hashPass = $row["hashPassword"];
+    }
+  } else {
+    echo "Wrong username!";
+  }
+
+  if (!validate_password($password, $hashPass)) {
+    echo "<script>alert('Username or password is wrong, please try again!');location='http://127.0.0.1:5500/frontend/src/html/login.html'</script>";
   } else {
     echo "<script>location='http://127.0.0.1:5500/frontend/src/html/home.html'</script>";
   };
 
   ?>
-
-</body>
-
-</html>
