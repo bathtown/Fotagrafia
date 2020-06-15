4149cd112e50735bd276fbb66d95ed6fa8cde440
hyf <hyf-0418@outlook.com>
2020-06-15 15:02:27 +0800

    add brower feature

--- backend/PHP/api/randomImg.php
+++ backend/PHP/api/randomImg.php
@@ -8,18 +8,8 @@
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die("Fatal Error");
 
-  // get Country_RegionCodeISO
-  $country = mysql_entities_fix_string($conn, $_GET['country']);
-  $countryiso = CountryRegionName2CountryRegionCodeISO($country);
-
-  // get CityCode
-  $city = mysql_entities_fix_string($conn, $_GET['city']);
-  $cityCode = CityName2CityCode($city);
-
-  $content = mysql_entities_fix_string($conn, $_GET['content']);
-
   // get imgs array
-  $img_query = "SELECT PATH, ImageID FROM travelimage WHERE Country_RegionCodeISO='$countryiso' AND CityCode='$cityCode' AND Content='$content'";
+  $img_query = "SELECT PATH FROM travelimage WHERE Country_RegionCodeISO='CA' LIMIT 10, 20";
   $img_result = $conn->query($img_query);
   if (!$img_result) die("Fatal Error");
 
@@ -27,10 +17,7 @@
   $imgs = array();
   for ($j = 0; $j < $img_rows; ++$j) {
     $img_row = $img_result->fetch_array(MYSQLI_ASSOC);
-    array_push($imgs, [
-      'src' => $img_row['PATH'],
-      'id' => $img_row['ImageID'],
-    ]);
+    array_push($imgs, $img_row['PATH']);
   }
   $img_result->close();
 
